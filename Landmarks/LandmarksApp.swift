ff57e12d231a1a9a6c9c2f15d962ce869dcbed47
Iñaki <inayabarb@gmail.com>
2023-08-08 15:21:02 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Iñaki Yabar Bilbao on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
